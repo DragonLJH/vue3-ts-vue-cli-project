922ab4e9a5c7e4d937e4357865f1b213073a7437
DragonLJH <1183665002@qq.com>
2022-10-21 18:29:24 +0800

    修改图片上传路径

--- src/components/CommomPage/QueryPage/myFormItem.vue
+++ src/components/CommomPage/QueryPage/myFormItem.vue
@@ -4,6 +4,7 @@
     v-model="item.value"
     placeholder="Please input"
   />
+  <MyArrayStr v-if="item.elName === 'ArrayStr'" v-model="item.value" />
   <el-input-number
     v-if="item.elName === 'ElInputNumber'"
     v-model="item.value"
@@ -11,11 +12,11 @@
     controls-position="right"
   />
   <el-upload
+    multiple
     v-if="item.elName === 'ElUploads'"
     v-model:file-list="item.value"
-    name="uploadProductRotationImg"
-    class="upload-demo"
-    action="/api/product/uploadProductRotationImg"
+    :name="'upload' + titleCase(item.name)"
+    :action="'/api/product/upload' + titleCase(item.name)"
     :on-success="handleSuccess"
     list-type="picture"
   >
@@ -24,29 +25,41 @@
   <el-upload
     v-if="item.elName === 'ElUpload'"
     v-model:file-list="item.value"
-    name="uploadProductRotationImg"
-    class="upload-demo"
-    action="/api/product/uploadProductRotationImg"
+    :name="'upload' + titleCase(item.name)"
+    :action="'/api/product/upload' + titleCase(item.name)"
     :on-success="handleSuccess"
     list-type="picture"
   >
-    <el-button type="primary">上传（支持多选文件）</el-button>
+    <el-button type="primary">上传 </el-button>
   </el-upload>
 </template>
 
 <script lang="ts">
-import { defineComponent, h, reactive } from "vue";
+import { defineComponent, h, reactive, toRefs } from "vue";
+import MyArrayStr from "./myArrayStr.vue";
 
 export default defineComponent({
   name: "myFormItem",
+  components: { MyArrayStr },
   props: {
     item: { type: Object },
   },
   setup(props: any) {
-    const { item, modelValue } = props;
-    console.log("myFormItem", item, modelValue);
+    const titleCase = (str: string) => {
+      return str.slice(0, 1).toUpperCase() + str.slice(1);
+    };
+    const handleSuccess = (
+      response: any,
+      uploadFile: any,
+      uploadFiles: any
+    ) => { 
+      // props.item.value = [...response];
+      console.log(props.item.value);
+    };
     return {
-      item,
+      ...toRefs(props),
+      titleCase,
+      handleSuccess,
     };
   },
 });
